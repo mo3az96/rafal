68343218b1bfdd5dfdcd791f2973f10def1d2b6b
mo3az.abdellatif <mo3az.abdellatif@gmail.com>
2020-02-25 12:34:56 +0200

    1st commit

---
+++ js/main.js
@@ -0,0 +1,111 @@
+$(window).on("load", function () {
+    //map
+    var adresse = "<img style='width:50px; text-align: left; display:inline-block; margin-right: 10px; vertical-align: sub;' src='img/logo.png'>";
+
+
+    var location = [adresse[0], $("#map").attr("lat"), $("#map").attr("long")];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+
+        center: new google.maps.LatLng($("#map").attr("lat"), $("#map").attr("long")),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoomControl: false,
+        fullscreenControl: false
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker;
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(location[1], location[2]),
+        map: map,
+        icon: 'images/pin.png',
+
+    });
+
+    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+        return function () {
+            infowindow.setContent(location[0]);
+            infowindow.open(map, marker);
+        }
+    })(marker));
+});
+
+
+$(document).ready(function () {
+    $('.menubtn').click(function () {
+        $(this).toggleClass('open');
+        $(".lang").toggleClass('open');
+        $("nav").fadeToggle("300");
+        $(".nav-ul").toggleClass('open');
+        if ($(window).width() <= 767) {
+            $("body").toggleClass("overflow");
+        }
+    });
+
+
+    if ($(window).width() <= 991) {
+        $(".services .service").wrap("<div class='item'></div>");
+        $(".services").addClass("owl-carousel");
+        $(".gallery .gallery-img").wrap("<div class='item'></div>");
+        $(".gallery").addClass("owl-carousel");
+        $(".clients .client").wrap("<div class='item'></div>");
+        $(".clients").addClass("owl-carousel");
+
+        $('.clients').owlCarousel({
+            items: 2,
+            margin: 30,
+            rtl: document.dir == 'rtl' ? true : false,
+            loop: true,
+            rewind: true,
+            autoplay: false,
+            nav: false,
+            dots: true,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                500: {
+                    items: 2,
+                },
+            }
+        });
+        $('.gallery').owlCarousel({
+            items: 2,
+            margin: 30,
+            rtl: document.dir == 'rtl' ? true : false,
+            loop: true,
+            rewind: true,
+            autoplay: false,
+            nav: false,
+            dots: true,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                500: {
+                    items: 2,
+                },
+            }
+        });
+        $('.services').owlCarousel({
+            items: 2,
+            margin: 30,
+            rtl: document.dir == 'rtl' ? true : false,
+            loop: true,
+            rewind: true,
+            autoplay: false,
+            nav: false,
+            dots: true,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                500: {
+                    items: 2,
+                },
+            }
+        });
+    }
+});
